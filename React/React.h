d10e5aeaae1bb11e80c6d55cfaf3d21a57719620
James Ide <ide@jameside.com>
2015-02-17 00:34:01 -0800

    [Headers] Add an umbrella header for the library
    The umbrella header allows projects that link React Native using CocoaPods to import it in standard fashion: `#import <React/React.h>`. Only some headers are included based on what I've found to be useful.
    
    Generated with `scripts/generate-umbrella-header.sh`, which is a script that scans the Base and Executors directories for all headers and also includes some handpicked Views headers as well.

---
+++ React/React.h
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "RCTAssert.h"
+#import "RCTBridge+Private.h"
+#import "RCTBridge.h"
+#import "RCTBridgeDelegate.h"
+#import "RCTBridgeMethod.h"
+#import "RCTBridgeModule.h"
+#import "RCTConvert.h"
+#import "RCTDefines.h"
+#import "RCTEventDispatcher.h"
+#import "RCTFrameUpdate.h"
+#import "RCTImageSource.h"
+#import "RCTInvalidating.h"
+#import "RCTJavaScriptExecutor.h"
+#import "RCTJavaScriptLoader.h"
+#import "RCTJSMethodRegistrar.h"
+#import "RCTKeyboardObserver.h"
+#import "RCTKeyCommands.h"
+#import "RCTLog.h"
+#import "RCTModuleData.h"
+#import "RCTModuleMethod.h"
+#import "RCTNullability.h"
+#import "RCTParserUtils.h"
+#import "RCTPerformanceLogger.h"
+#import "RCTRootView.h"
+#import "RCTRootViewDelegate.h"
+#import "RCTRootViewInternal.h"
+#import "RCTTouchHandler.h"
+#import "RCTURLRequestDelegate.h"
+#import "RCTURLRequestHandler.h"
+#import "RCTUtils.h"
+
+#import "RCTJSCExecutor.h"
+
+#import "RCTAccessibilityManager.h"
+#import "RCTAlertManager.h"
+#import "RCTAppState.h"
+#import "RCTAsyncLocalStorage.h"
+#import "RCTClipboard.h"
+#import "RCTDevLoadingView.h"
+#import "RCTDevMenu.h"
+#import "RCTExceptionsManager.h"
+#import "RCTRedBox.h"
+#import "RCTSourceCode.h"
+#import "RCTStatusBarManager.h"
+#import "RCTTiming.h"
+#import "RCTUIManager.h"
+
+#import "RCTAnimationType.h"
+#import "RCTAutoInsetsProtocol.h"
+#import "RCTComponent.h"
+#import "RCTConvert+CoreLocation.h"
+#import "RCTConvert+MapKit.h"
+#import "RCTPointerEvents.h"
+#import "RCTScrollableProtocol.h"
+#import "RCTShadowView.h"
+#import "RCTView.h"
+#import "RCTViewControllerProtocol.h"
+#import "RCTViewManager.h"
+#import "UIView+React.h"
