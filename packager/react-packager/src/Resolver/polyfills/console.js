989e7d7bfa8b99a05fda8ae2ebddfcc5f457eda2
James Ide <ide@jameside.com>
2015-10-09 23:46:32 -0700

    [Console] Keep the original console as `global.originalConsole`
    If a console exists, keep the original as `global.originalConsole` before overwriting `global.console` with a polyfill. This matches what we do for XHR, fetch, and some other libraries.
    
    Test Plan: console.log still tees to both consoles. `originalConsole.log` prints only to the Web Inspector console.

--- packager/react-packager/src/Resolver/polyfills/console.js
+++ packager/react-packager/src/Resolver/polyfills/console.js
@@ -366,9 +366,6 @@
   };
 
   function setupConsole(global) {
-
-    var originalConsole = global.console;
-
     if (!global.nativeLoggingHook) {
       return;
     }
@@ -455,7 +452,14 @@
       global.nativeLoggingHook('\n' + table.join('\n'), LOG_LEVELS.info);
     }
 
-    global.console = {
+    // Preserve the original `console` as `originalConsole`
+    var originalConsole = global.console;
+    var descriptor = Object.getOwnPropertyDescriptor(global, 'console');
+    if (descriptor) {
+      Object.defineProperty(global, 'originalConsole', descriptor);
+    }
+
+    var console = {
       error: getNativeLogFunction(LOG_LEVELS.error),
       info: getNativeLogFunction(LOG_LEVELS.info),
       log: getNativeLogFunction(LOG_LEVELS.info),
@@ -463,17 +467,18 @@
       trace: getNativeLogFunction(LOG_LEVELS.trace),
       table: consoleTablePolyfill
     };
+    Object.defineProperty(global, 'console', {...descriptor, value: console});
 
     // If available, also call the original `console` method since that is
     // sometimes useful. Ex: on OS X, this will let you see rich output in
     // the Safari Web Inspector console.
     if (__DEV__ && originalConsole) {
-      Object.keys(global.console).forEach(methodName => {
-        var reactNativeMethod = global.console[methodName];
+      Object.keys(console).forEach(methodName => {
+        var reactNativeMethod = console[methodName];
         if (originalConsole[methodName]) {
-          global.console[methodName] = function() {
+          console[methodName] = function() {
             originalConsole[methodName](...arguments);
-            reactNativeMethod.apply(global.console, arguments);
+            reactNativeMethod.apply(console, arguments);
           };
         }
       });
