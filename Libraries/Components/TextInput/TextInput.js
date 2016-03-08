ad138e233bc05f2397b68fa6ba7ba3ddbac9cc38
Brent Vatne <brentvatne@gmail.com>
2016-03-07 17:24:51 -0800

    Add onChangeContentSize callback

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -234,6 +234,10 @@ var TextInput = React.createClass({
      */
     onChange: PropTypes.func,
     /**
+     * Callback that is called when the text input's content size changes.
+     */
+    onChangeContentSize: PropTypes.func,
+    /**
      * Callback that is called when the text input's text changes.
      * Changed text is passed as an argument to the callback handler.
      */
@@ -522,6 +526,13 @@ var TextInput = React.createClass({
       };
     }
 
+    var onChangeContentSize;
+    if (this.props.onChangeContentSize) {
+      onChangeContentSize = (event: Event) => {
+        this.props.onChangeContentSize(event);
+      }
+    }
+
     var autoCapitalize =
       UIManager.AndroidTextInput.Constants.AutoCapitalizationType[this.props.autoCapitalize];
     var children = this.props.children;
@@ -549,6 +560,7 @@ var TextInput = React.createClass({
         onFocus={this._onFocus}
         onBlur={this._onBlur}
         onChange={this._onChange}
+        onChangeContentSize={onChangeContentSize}
         onSelectionChange={onSelectionChange}
         onTextInput={this._onTextInput}
         onEndEditing={this.props.onEndEditing}
